60d13bacd05e441ab0c42f3673ca2a0bb9af331c
Babatunde Jubril <babatundejubril@gmail.com>
2022-09-13 14:06:15 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putcher('p');
+_putcher('u');
+_putcher('t');
+_putcher('c');
+_putcher('h');
+_putcher('a');
+_putcher('r');
+_putcher('\n');
+
+return (0);
+}
