19284437ec09fee2ca6479fbf0c9d8c3fce17a02
Babatunde Jubril <babatundejubril@gmail.com>
2022-09-14 02:48:12 -0700

    task-6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,24 +1,16 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * print_sign - function to check for a sign of a number
- * @n:  is the int that will use for the argument of the function
+ * _abs - function that computes the absolute value of an integer
+ * @c:  is the int that will use for the argument of the function
  * Return: 0
  */
-int print_sign(int n)
+int _abs(int c)
 {
-if (n > 0)
+if (c > 0 || c == 0)
 {
-_putchar('+');
-return (1);
-}
-else if (n < 0)
-{
-_putchar('-');
-return (-1);
+return (c);
 }
 else
-{
-_putchar('0');
-return (0);
-}
+return (c * -1);
 }
