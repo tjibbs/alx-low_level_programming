3cbe5a3c3494bc15078308e9347cad05bd10dbb5
Babatunde Jubril <babatundejubril@gmail.com>
2022-09-26 15:22:00 -0700

    task-0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,28 @@
-#include "holberton.h"
-/**
- * _memset - fills memory with constant byte.
- * @s: array of char
- * @b: array of chat
- * @n: number of bytes
- * Return:(s)
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-char *p = s;
-while (n > 0)
-{
-*p = b;
-p++;
-n--;
-}
-return (s);
+/*                                                                                                                                   
+ * File: 0-memset.c                                                                                                                  
+ * Auth: not                                                                                                                         
+ */                                                                                                                                  
+                                                                                                                                     
+#include "main.h"                                                                                                                    
+                                                                                                                                     
+/**                                                                                                                                  
+ * _memset - Fills the first n bytes of the memory area                                                                              
+ *          pointed to by @s with the constant byte @c.                                                                              
+ * @s: A pointer to the memory area to be filled.                                                                                    
+ * @c: The character to fill the memory area with.                                                                                   
+ * @n: The number of bytes to be filled.                                                                                             
+ * description _memset: over there                                                                                                   
+ *                                                                                                                                   
+ * Return: A pointer to the filled memory area @s.                                                                                   
+ *                                                                                                                                   
+ */                                                                                                                                  
+void *_memset(void *s, int c, size_t n)                                                                                              
+{                                                                                                                                    
+        unsigned int index;                                                                                                          
+        unsigned char *memory = s, value = c;                                                                                        
+                                                                                                                                     
+        for (index = 0; index < n; index++)                                                                                          
+                memory[index] = value;                                                                                               
+                                                                                                                                     
+        return (memory);                                                                                                             
 }
