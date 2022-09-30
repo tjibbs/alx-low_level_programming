0db7f8b5aca2241e4896aebbb7631754753fc7c9
Babatunde Jubril <babatundejubril@gmail.com>
2022-09-30 00:56:16 -0700

    task-0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - this rints a string, then a new line.
+ * @s: -string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+		_putchar('\n');
+}
