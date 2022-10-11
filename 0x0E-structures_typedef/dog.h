b09705990718257dce60a4a93488f5f37cb758b8
Babatunde Jubril <babatundejubril@gmail.com>
2022-10-10 17:30:05 -0700

    task-0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,5 +22,31 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct dog
+ * @name - name
+ * @age - age
+ * @owner - owner
+ * Discription: this struct is for dog
+ */
+
+struct dog
+{
+        char *name;
+        float age;
+        char *owner
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* DOG_H */
 
 #endif /* DOG_H */
